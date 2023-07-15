f0b47c3fe82bd198e4a9aa2913a6fcc0b7564975
irina Gromik <irina.gromik@gmail.com>
2023-07-16 02:08:48 +0300

    Initial commit

---
+++ Hacker_tasks.py
@@ -0,0 +1,15 @@
+# print('Hello, print your name...')
+# name = str(input())
+# print(f'Guten tag {name}')
+
+# numbers = input('Hello, print numbers ...')
+# numbers_list = numbers.split()
+# numbers_list = [int(num) for num in numbers_list]
+# sort_numbers = sorted(numbers_list)
+# print(sort_numbers[-1])
+
+# n = int(input('Hello, print numbers ...'))
+# if n % 2 == 0:
+#     print(n, 'is even ')
+# elif n % 2 != 0:
+#     print(n, 'is odd')
--- l14.py
+++ l14.py
@@ -1,5 +1,8 @@
+import nums as nums
+
+
 def find_max(nums):
-    max_num = float("-inf") # smaller than all other numbers
+    max_num = float("inf") # smaller than all other numbers
 for num in nums:
     if num > max_num:
 # (Fill in the missing line here)
